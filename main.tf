48acdf599eda125f691804f989462f69c889ea1b
lekkalahyma <lekkalahyma2001@gmail.com>
2022-10-19 15:37:46 +0530

    sample

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 resource "aws_vpc" "dev" {
-  cidr_block       = "var.cidr_block"
+  cidr_block       = " var.vpc_cidr"
   instance_tenancy = "default"
   enable_dns_hostnames = "true"
   enable_dns_support = "true"
--- variable.tf
+++ variable.tf
@@ -1,3 +1,4 @@
-variable "cidr_block" {
-  default     = "10.1.0.0/16" 
+variable "vpc_cidr" {
+  default = "10.1.0.0/16"
+
 }
